d2895b57d483959957ddea66de3e35ef1f6c406a
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-05-25 18:28:39 +0530

    testing components in vite

--- frontend_vite/src/App.jsx
+++ frontend_vite/src/App.jsx
@@ -1,13 +1,16 @@
+import Test from "./Test";
+
 function App() {
   return (
- <>
-     <div>
-      <h1 style={{ textAlign: "center" }}>
-        Hello React with Vite | Archishmaan Upadhyaya
-      </h1>
-    </div>
- </>
-  )
+    <>
+      <div>
+        <h1 style={{ textAlign: "center" }}>
+          Hello React with Vite | Archishmaan Upadhyaya
+        </h1>
+      </div>
+      <Test />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend_vite/src/Test.jsx
@@ -0,0 +1,9 @@
+function Test() {
+  return (
+    <div>
+      <h3 style={{ textAlign: "center" }}>Testing component!</h3>
+    </div>
+  );
+}
+
+export default Test;
